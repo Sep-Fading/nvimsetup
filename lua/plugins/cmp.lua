d8997df0753134f73c70e2dfe89ef02fe54f4017
Sepehr <personal.sepehr@gmail.com>
2025-10-12 23:09:36 +0100

    auto complete fix: no more double inserts

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -5,26 +5,25 @@ cmp.setup({
   snippet = {
     expand = function(args)
       luasnip.lsp_expand(args.body)
-      vim.fn["vsnip#anonymous"](args.body)
+      --vim.fn["vsnip#anonymous"](args.body)
     end,
   },
   mapping = cmp.mapping.preset.insert({
     ["<Tab>"] = cmp.mapping.select_next_item(),
     ["<S-Tab>"] = cmp.mapping.select_prev_item(),
     ["<CR>"] = cmp.mapping.confirm({ 
-        behaviour = cmp.ConfirmBehavior.Insert,
-        select = true }),
+        select = false}),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.close(),
   }),
   sources = cmp.config.sources({
-    { name = "nvim_lsp", keyword_length=3 },
+    { name = "nvim_lsp", keyword_length=2 },
     { name = "luasnip" },
     { name = "path"},
     { name = "nvim_lsp_signature_help"},
     { name = "nvim_lua", keyword_length = 2},
-    { name = "vsnip", keyword_length = 2},
-    { name = "buffer", keyword_length = 3},
+    --{ name = "vsnip", keyword_length = 2},
+    { name = "buffer", keyword_length = 2},
     { name = "calc"},
   }),
 
